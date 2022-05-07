5a7fa886e2fa0c0e6ef92b810c17c1cc027b2b8b
PureFunctor <purefunctor@gmail.com>
2022-05-07 15:17:02 +0800

    Add mouse position as header

--- src/Main.purs
+++ src/Main.purs
@@ -4,24 +4,37 @@ import Prelude
 
 import Control.Alt ((<|>))
 import Deku.Attribute (class Attr, Attribute, (:=))
-import Deku.Control (text_)
+import Deku.Control (text, text_)
+import Deku.Core (Element)
 import Deku.DOM (Style)
 import Deku.DOM as D
 import Deku.Toplevel (runInBodyA)
 import Effect (Effect)
+import FRP.Behavior (sample)
+import FRP.Behavior.Mouse (position)
 import FRP.Event (Event, makeEvent, subscribe)
+import FRP.Event.AnimationFrame (animationFrame)
 import FRP.Event.Keyboard (down, up)
+import FRP.Event.Mouse (Mouse)
+import FRP.Event.Mouse as Mouse
+import Data.Maybe
+
+type Env =
+  { mouse :: Mouse
+  }
 
 main :: Effect Unit
-main = runInBodyA view
-  where
-  view :: forall lock. _ (_ lock _)
-  view =
-    [ D.header_ [ text_ "Deku Tree" ]
-    , D.main_ $ [ "D", "F", "J", "K" ] <#> \key ->
-        D.div (keyDownEvent key <|> keyUpEvent key) [ text_ key ]
-    , D.footer_ [ text_ "PureFunctor, 2022 <|> Made with <3 and FRP" ]
-    ]
+main = do
+  mouse <- Mouse.getMouse
+  runInBodyA (view { mouse })
+
+view :: forall lock payload. Env -> Array (Element lock payload)
+view { mouse } =
+  [ D.header_ [ text $ mouseMoveEvent mouse ]
+  , D.main_ $ [ "D", "F", "J", "K" ] <#> \key ->
+      D.div (keyDownEvent key <|> keyUpEvent key) [ text_ key ]
+  , D.footer_ [ text_ "PureFunctor, 2022 <|> Made with <3 and FRP" ]
+  ]
 
 keyDownEvent :: forall e. Attr e Style String => String -> Event (Attribute e)
 keyDownEvent key = makeEvent \k ->
@@ -38,3 +51,6 @@ keyUpEvent key = makeEvent \k ->
       k $ D.Style := (mempty :: String)
     else
       pure unit
+
+mouseMoveEvent :: Mouse -> Event String
+mouseMoveEvent mouse = Mouse.withPosition mouse animationFrame <#> _.pos >>> maybe "No Mouse!" show
