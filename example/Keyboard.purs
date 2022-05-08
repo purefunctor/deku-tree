d81ef5cb08793c6f36b8b2977841d448cf86b68a
PureFunctor <purefunctor@gmail.com>
2022-05-09 00:53:14 +0800

    Add counters to the keyboard example

--- example/Keyboard.purs
+++ example/Keyboard.purs
@@ -4,10 +4,11 @@ import Prelude
 
 import Control.Alt ((<|>))
 import Deku.Attribute (class Attr, Attribute, (:=))
-import Deku.Control (blank, plant, text_)
+import Deku.Control (blank, plant, text, text_)
 import Deku.Core (Domable)
 import Deku.DOM (Style)
 import Deku.DOM as D
+import Effect.Ref as Ref
 import FRP.Event (Event, bang, makeEvent, subscribe)
 import FRP.Event.Keyboard (down, up)
 
@@ -18,29 +19,48 @@ view = plant $
       ]
   , D.hr_ blank
   , D.section (bang $ D.Class := "keyboard-container-row")
-      [ D.div (keyDownEvent "KeyS" <|> keyUpEvent "KeyS") [ text_ "S" ]
-      , D.div (keyDownEvent "KeyD" <|> keyUpEvent "KeyD") [ text_ "D" ]
-      , D.div (keyDownEvent "KeyF" <|> keyUpEvent "KeyF") [ text_ "F" ]
-      , D.div (keyDownEvent "Space" <|> keyUpEvent "Space") [ text_ "Space" ]
-      , D.div (keyDownEvent "KeyJ" <|> keyUpEvent "KeyJ") [ text_ "J" ]
-      , D.div (keyDownEvent "KeyK" <|> keyUpEvent "KeyK") [ text_ "K" ]
-      , D.div (keyDownEvent "KeyL" <|> keyUpEvent "KeyL") [ text_ "L" ]
+      [ D.div (keyStyle "KeyS") [ text_ "S" ]
+      , D.div (keyStyle "KeyD") [ text_ "D" ]
+      , D.div (keyStyle "KeyF") [ text_ "F" ]
+      , D.div (keyStyle "Space") [ text_ "Space" ]
+      , D.div (keyStyle "KeyJ") [ text_ "J" ]
+      , D.div (keyStyle "KeyK") [ text_ "K" ]
+      , D.div (keyStyle "KeyL") [ text_ "L" ]
+      ]
+  , D.hr_ blank
+  , D.section (bang $ D.Class := "keyboard-container-row")
+      [ D.div_ [ text $ ctrState "KeyS" <#> show ]
+      , D.div_ [ text $ ctrState "KeyD" <#> show ]
+      , D.div_ [ text $ ctrState "KeyF" <#> show ]
+      , D.div_ [ text $ ctrState "Space" <#> show ]
+      , D.div_ [ text $ ctrState "KeyJ" <#> show ]
+      , D.div_ [ text $ ctrState "KeyK" <#> show ]
+      , D.div_ [ text $ ctrState "KeyL" <#> show ]
       ]
   , D.hr_ blank
   ]
+  where
+  keyStyle :: forall e. Attr e Style String => String -> Event (Attribute e)
+  keyStyle keyCode = makeEvent \k -> do
+    downC <- subscribe down \keyCode' ->
+      when (keyCode == keyCode')
+        $ k
+        $ D.Style := "background-color: white; color: black;"
+    upC <- subscribe up \keyCode' ->
+      when (keyCode == keyCode')
+        $ k
+        $ D.Style := ""
+    pure do
+      downC
+      upC
 
-keyDownEvent :: forall e. Attr e Style String => String -> Event (Attribute e)
-keyDownEvent keyCode = makeEvent \k ->
-  subscribe down \keyCode' ->
-    if keyCode == keyCode' then
-      k $ D.Style := "background-color: white; color: black;"
-    else
-      pure unit
-
-keyUpEvent :: forall e. Attr e Style String => String -> Event (Attribute e)
-keyUpEvent keyCode = makeEvent \k ->
-  subscribe up \keyCode' ->
-    if keyCode == keyCode' then
-      k $ D.Style := (mempty :: String)
-    else
-      pure unit
+  ctrState :: String -> Event Int
+  ctrState keyCode = bang 0 <|> increment
+    where
+    increment = makeEvent \k -> do
+      current <- Ref.new 1
+      subscribe down \keyCode' ->
+        when (keyCode == keyCode') do
+          current' <- Ref.read current
+          k current'
+          Ref.modify_ (_ + 1) current
